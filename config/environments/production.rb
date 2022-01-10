b54f35809f73696c4178c30137e86aec872cddac
Lorenzo <progpyftk@yahoo.com>
2022-01-10 18:46:19 -0300

    keep working on email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,20 +65,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'vast-waters-03159.herokuapp.com' # colocar o nome do app aqui
-  config.action_mailer.default_url_options = { host: host }
+
+  # Heroku's addon - Mailer To Go - Configuration
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    # note that they are accessed via the ENV environment variable instead of being hard-coded
-    # $ heroku config:get SENDGRID_USERNAME
-    # $ heroku config:get SENDGRID_PASSWORD
-    user_name: 'apikey',
-    password:  ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'vast-waters-03159.heroku.com',
+    authentication: :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
