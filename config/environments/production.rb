9d680daa6ea04efed117d8c3374000c4debb4d05
Lorenzo <progpyftk@yahoo.com>
2022-01-10 18:56:31 -0300

    email...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "heroku.com" }
 
   # Heroku's addon - Mailer To Go - Configuration
   ActionMailer::Base.smtp_settings = {
